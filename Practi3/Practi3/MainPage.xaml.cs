b0d0af07f35ba20b75d80048a01c4c0feb6f1380
BRENDA <BRENDA@DESKTOP-7O4U7A5>
2017-09-18 18:39:16 -0500

    Agregar archivos de proyecto.

---
+++ Practi3/Practi3/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practi3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Datos Ingresados", Nombre.Text + " " + Apellido.Text + "\n " + Direccion.Text + " \n " + TEL.Text + " \n" + Carrera.Text + " \n" + Semestre.Text + "\n " + Matricula.Text + "\n " + Correo.Text + "\n " + GitHub.Text, "OK");
+        }
+    }
+}
